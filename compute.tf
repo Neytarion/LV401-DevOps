969cca521cf61a24db80048569cbe6120fc8c311
Neytarion <47857375+Neytarion@users.noreply.github.com>
2019-06-22 15:00:57 +0300

    Update compute.tf

--- compute.tf
+++ compute.tf
@@ -1,20 +1,14 @@
 ### To Provision FireWall rule ###
-
 resource "google_compute_firewall" "www" {
   name = "jenkins-lv401-firewall"
   network = "default"
-
   allow {
     protocol = "tcp"
     ports = ["8080", "443"]
   }
-
   source_ranges = ["0.0.0.0/0"]
 }
-
-
 ### To provision Jenkins Master ###
-
 resource "google_compute_instance" "jenkins-master-1" {
    name = "jenkins-master-lv401"
    machine_type = "n1-standard-1"
@@ -22,7 +16,7 @@ resource "google_compute_instance" "jenkins-master-1" {
    tags = ["jenkins"]
    boot_disk {
       initialize_params {
-      image = "centos-7"
+      image = "debian-cloud/debian-9"
    }
 }
 network_interface {
@@ -32,10 +26,5 @@ network_interface {
 service_account {
    scopes = ["userinfo-email", "compute-ro", "storage-ro"]
    }
-
- metadata_startup_script = "sudo yum install -y wget"
-}
-
-output "public_ip_master" {
-   value = ["${google_compute_instance.jenkins-master-1.*.network_interface.0.access_config.0.assigned_nat_ip}"]
+ metadata_startup_script = "sudo apt-get update;sudo apt install -y openjdk-8-jdk ;sudo apt-get install -y apt-transport-https; wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -; sudo sh -c 'echo deb https:/$
 }
